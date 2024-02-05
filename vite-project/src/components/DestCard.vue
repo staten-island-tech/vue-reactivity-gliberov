7a4721e34a015d9a2bea2602ee8c316cda13fc54
gliberov <124058342+gliberov@users.noreply.github.com>
2024-02-05 13:11:21 -0500

    yes

---
+++ vite-project/src/components/DestCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+<h2>{{  Destination.name }}</h2>
+<img :src="Destination.img" alt="" />
+<h3>{{ clicked }}</h3>
+<button @clicked="increment">Click Me</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+    const props = defineProps({
+        Destination: object,
+    });
+
+    const clicked = ref(0)
+    function increment() {
+        clicked.value++
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
