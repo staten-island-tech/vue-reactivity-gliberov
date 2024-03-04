83ffa0d3d12931ecc5d7b6dba1e7c049fe3f146b
gliberov <124058342+gliberov@users.noreply.github.com>
2024-03-03 22:51:23 -0500

    yes

--- vite-project/src/components/Piano.vue
+++ vite-project/src/components/Piano.vue
@@ -128,31 +128,5 @@ export default {
   background-color: #ffcc00;
 }
 
-.replay {
-  background-color: #4CAF50; 
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  transition-duration: 0.4s;
-}
 
-.reset {
-  background-color: #f44336; 
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  transition-duration: 0.4s;
-}
 </style>
